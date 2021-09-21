a192ef31bfd052b249c96f00511f55e109ebcff3
Kevin-Chatelain <73096751+Kevin-Chatelain@users.noreply.github.com>
2021-09-21 16:40:34 +0200

    Delete TP3 directory

--- TP3/js/script.js
+++
@@ -1,7 +0,0 @@
-function salut() {
-    document.querySelector(".container").innerHTML += "<p>Haha salut</p>";
-}
-
-function cv() {
-    document.querySelector(".container").innerHTML += "<p>ah ouais ok</p>";
-}
