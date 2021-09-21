e7f40832a65d0e0b10133a27e68f48dd81469fcb
Kevin-Chatelain <73096751+Kevin-Chatelain@users.noreply.github.com>
2021-09-21 16:41:05 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function salut() {
+    document.querySelector(".container").innerHTML += "<p>Haha salut</p>";
+}
+
+function cv() {
+    document.querySelector(".container").innerHTML += "<p>ah ouais ok</p>";
+}
